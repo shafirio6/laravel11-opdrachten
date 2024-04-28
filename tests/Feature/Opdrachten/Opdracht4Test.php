0ea1e18c4d0406437930145206a14bf5bde896e4
Koningstein <m.koningstein@tcrmbo.nl>
2024-04-28 21:10:53 +0200

    Update testen van opdracht4

--- tests/Feature/Opdrachten/Opdracht4Test.php
+++ tests/Feature/Opdrachten/Opdracht4Test.php
@@ -2,17 +2,44 @@
 
 use App\Models\Project;
 
-beforeEach(function (){
-    $this->project = Project::factory()->create();
-    $this->seed('ProjectSeeder');
-});
-
-test('An project admin index page is visable with projects on the page', function()
+test('An project admin index page is visable ', function()
 {
     $this->withoutExceptionHandling();
+
+    // Testen
     $this->get(route('projects.index'))
         ->assertViewIs('admin.projects.index')
-        ->assertSee($this->project->id)
-        ->assertSee($this->project->name)
         ->assertStatus(200);
 })->group('Opdracht4');
+
+test('The project admin index page shows all projects', function() {
+    $this->withoutExceptionHandling();
+    // Data aanmaken
+    $projects = Project::factory()->count(15)->create();
+
+    // Testen
+    $response = $this->get(route('projects.index'))->assertViewIs('admin.projects.index');
+    foreach ($projects as $project) {
+        $response->assertSee((string)$project->id);
+        $response->assertSee($project->name); // Voeg false toe als je exacte matches wilt vermijden
+    }
+    $response->assertStatus(200);
+})->group('Opdracht4');
+
+test('The project admin index page shows all projects from Seeder', function() {
+    // Data aanmaken
+    $this->withoutExceptionHandling();
+    $this->seed('ProjectSeeder');
+    $projects = Project::all();
+
+    // Testen
+    $response = $this->get(route('projects.index'))->assertViewIs('admin.projects.index');
+    foreach ($projects as $project) {
+        $response->assertSee((string)$project->id);
+        $response->assertSee($project->name); // Voeg false toe als je exacte matches wilt vermijden
+    }
+    $response->assertStatus(200);
+})->group('Opdracht4');
+
+
+

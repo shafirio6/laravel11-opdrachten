677512df55f05b75740beb6743c055f107458e12
Koningstein <m.koningstein@tcrmbo.nl>
2024-06-27 12:12:28 +0200

    Test 11 verder uitgewerkt

--- tests/Feature/Opdrachten/Opdracht11Test.php
+++ tests/Feature/Opdrachten/Opdracht11Test.php
@@ -8,76 +8,63 @@ use \Pest\Laravel;
 beforeEach(function (){
 //    $this->seed('RoleAndPermissionSeeder');
 //    $this->seed('UserSeeder');
-    $this->project = Project::factory()->create();
+
+    $this->projects = Project::factory()->count(15)->create();
     $this->seed('ProjectSeeder');
+    $this->project = Project::factory()->create();
 });
 
-test('the visitor can view the public projects page', function()
-{
-    $this->withoutExceptionHandling();
-    $this
-        ->get(route('open.projects.index'))
-        ->assertViewIs('open.projects.index')
-        ->assertSee($this->project->id)
-        ->assertSee($this->project->name)
-        ->assertSee(Str::limit($this->project->description, 350))
-        ->assertStatus(200);
+test('the visitor can view the public projects page', function() {
+    $response = $this->get(route('open.projects.index'));
+
+    $response->assertStatus(200);
+})->group('Opdracht11');
+
+test('the projects page uses the correct view', function() {
+    $response = $this->get(route('open.projects.index'));
+
+    $response->assertViewIs('open.projects.index');
+})->group('Opdracht11');
+
+test('the route name open.projects.index points to /projects', function() {
+    $this->assertEquals(url('/projects'), route('open.projects.index'));
+})->group('Opdracht11');
+
+test('the public projects page shows project details', function() {
+    $response = $this->get(route('open.projects.index'));
+
+    $response->assertSee($this->projects->first()->id);
+    $response->assertSee($this->projects->first()->name);
+    $response->assertSee(Str::limit($this->projects->first()->description, 350));
+})->group('Opdracht11');
+
+test('the public projects page shows paginated projects', function() {
+    $response = $this->get(route('open.projects.index'));
+
+    // Ensure only 10 projects are shown on the first page
+    $projectsOnFirstPage = $this->projects->take(10);
+    $projectsOnFirstPage->each(function ($project) use ($response) {
+        $response->assertSee($project->name);
+    });
+
+    $projectsNotOnFirstPage = $this->projects->skip(10);
+    $projectsNotOnFirstPage->each(function ($project) use ($response) {
+        $response->assertDontSee($project->name); // This should be on the second page
+    });
+
+    $response = $this->get(route('open.projects.index', ['page' => 2]));
+    $projectsOnSecondPage = $this->projects->skip(10)->take(5);
+    $projectsOnSecondPage->each(function ($project) use ($response) {
+        $response->assertSee($project->name); // This should be on the second page
+    });
+})->group('Opdracht11');
+
+test('the public projects page shows pagination links', function() {
+    $response = $this->get(route('open.projects.index'));
+
+    $response->assertSee('<nav role="navigation" aria-label="Pagination Navigation"', false);
 })->group('Opdracht11');
 
 
 
 
-
-//
-//test('the visitor can view the public projects page', function()
-//{
-//    $this->withoutExceptionHandling();
-//    $this
-//        ->get(route('public.projects.index'))
-//        ->assertViewIs('open.projects.index')
-//        ->assertSee($this->project->id)
-//        ->assertSee($this->project->name)
-//        ->assertSee(Str::limit($this->project->description, 350))
-//        ->assertStatus(200);
-//})->group('Opdracht11');
-//
-//test('the student user can view the public projects page', function()
-//{
-//    $this->withoutExceptionHandling();
-//    $user = User::find(1);
-//    Laravel\be($user)
-//        ->get(route('public.projects.index'))
-//        ->assertViewIs('open.projects.index')
-//        ->assertSee($this->project->id)
-//        ->assertSee($this->project->name)
-//        ->assertSee(Str::limit($this->project->description, 350))
-//        ->assertStatus(200);
-//})->group('Opdracht11');
-//
-//test('the teacher user can view the public projects page', function()
-//{
-//    $this->withoutExceptionHandling();
-//    $user = User::find(2);
-//    Laravel\be($user)
-//        ->get(route('public.projects.index'))
-//        ->assertViewIs('open.projects.index')
-//        ->assertSee($this->project->id)
-//        ->assertSee($this->project->name)
-//        ->assertSee(Str::limit($this->project->description, 350))
-//        ->assertStatus(200);
-//})->group('Opdracht11');
-//
-//test('the admin user can view the public projects page', function()
-//{
-//    $this->withoutExceptionHandling();
-//    $user = User::find(3);
-//    Laravel\be($user)
-//        ->get(route('public.projects.index'))
-//        ->assertViewIs('open.projects.index')
-//        ->assertSee($this->project->id)
-//        ->assertSee($this->project->name)
-//        ->assertSee(Str::limit($this->project->description, 350))
-//        ->assertStatus(200);
-//})->group('Opdracht11');
-//
-//

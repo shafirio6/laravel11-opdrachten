011af457bf2678e218c3ad86be3fc3444cd7f773
Koningstein <m.koningstein@tcrmbo.nl>
2024-06-27 12:08:57 +0200

    Test 9 verder uitgewerkt

--- tests/Feature/Opdrachten/Opdracht9Test.php
+++ tests/Feature/Opdrachten/Opdracht9Test.php
@@ -17,8 +17,43 @@ test('delete project page is visable', function()
         ->assertStatus(200);
 })->group('Opdracht9');
 
+test('In de delete form is the correct action', function()
+{
+    $this->withoutExceptionHandling();
+    $expectedAction = route('projects.destroy',['project' => $this->project->id]);
+
+    $response = $this->get(route('projects.delete',['project' => $this->project->id]));
+    $response->assertViewIs('admin.projects.delete');
+
+    // Zorg ervoor dat de action URL correct is in het formulier
+    $response->assertSee('action="' . $expectedAction . '"', false);
+
+    $response->assertStatus(200);
+})->group('Opdracht9');
+
 test('a project can be deleted', function(){
     $this->json('DELETE', route('projects.destroy', ['project' => $this->project->id]))
         ->assertRedirect(route('projects.index'));
     $this->assertDatabaseMissing('projects', ['id' => $this->project->id]);
 })->group('Opdracht9');
+
+
+test('delete method redirects to index after successful store and shows flash message', function () {
+    // Post request naar store route met correcte data
+    $project = Project::factory()->create(['name' => 'testproject', 'description' => 'dit is een test project']);
+    $response = $this->json('DELETE', route('projects.destroy', ['project' => $project->id]))
+        ->assertRedirect(route('projects.index'));
+
+    // Assert of we een redirect status van 302 hebben (redirect)
+    $response->assertStatus(302);
+
+    // Assert of de melding met session wordt gebruikt
+    $response->assertSessionHas('status', 'Project testproject is verwijderd');
+
+    // volg de redirect en check of de flash message op het scherm staat
+    $this->followingRedirects()
+        ->get(route('projects.index'))
+        ->assertSee('Project testproject is verwijderd');
+})->group('Opdracht9');
+
+
